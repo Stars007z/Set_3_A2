cdf302981db7a3cfe805b0a6fa63e5974c861cde
Pavlukhin Denis Igorevich <145040543+Stars007z@users.noreply.github.com>
2024-11-25 00:23:11 +0300

    ed

--- A2_additional/main.cpp
+++ A2_additional/main.cpp
@@ -1,27 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <chrono>
-#include"array_generator.h"
-#include"sort_tester.h"
-#include"sorting_alg.h"
+#include "array_generator.h"
+#include "sort_tester.h"
+#include "sorting_alg.h"
 
 int main() {
     int n = 10000;
     auto rand_arr = ArrayGenerator::generateRandomArray(n);
-
     auto reverse_arr = ArrayGenerator::generateReverseSortedArray(n);
     auto nearly_arr = ArrayGenerator::generateNearlySortedArray(n);
-    auto rand_vect = ArrayGenerator::generateSubarrays(rand_arr);
-    auto reverse_vect = ArrayGenerator::generateSubarrays(rand_arr);
-    auto near_vect = ArrayGenerator::generateSubarrays(rand_arr);
 
-    for (int i = 0; i < rand_vect.size(); ++i) {
-        long long rand_time = SortTester::mSTime(rand_vect[i]);
-        long long reverse_time = SortTester::mSTime(reverse_vect[i]);
-        long long near_time = SortTester::mSTime(near_vect[i]);
-        std::cout << rand_vect[i].size() << " " << rand_time << " " << reverse_time << " " << near_time << '\n';
+    auto rand_vect = ArrayGenerator::generateSubarrays(rand_arr);
+    auto reverse_vect = ArrayGenerator::generateSubarrays(reverse_arr);
+    auto near_vect = ArrayGenerator::generateSubarrays(nearly_arr);
+
+    std::ofstream outFile("ms_results.txt");
+    if (!outFile.is_open()) {
+        std::cerr << "Не удалось открыть файл для записи!" << std::endl;
+        return 1;
     }
 
+    outFile << "Size Trand Treverse Tnear\n";
+    for (int i = 0; i < rand_vect.size(); ++i) {
+        long long rand_time = SortTester::averageTimeMS(rand_vect[i]);
+        long long reverse_time = SortTester::averageTimeMS(reverse_vect[i]);
+        long long near_time = SortTester::averageTimeMS(near_vect[i]);
 
+        outFile << rand_vect[i].size() << " " << rand_time << " " << reverse_time << " " << near_time << "\n";
+    }
+    outFile.close();
+    std::cout << "Результаты записаны в файл ms_results.txt" << std::endl;
+
+
+
+
+    std::ofstream outFile2("hms_results.txt");
+    if (!outFile2.is_open()) {
+        std::cerr << "Не удалось открыть файл для записи!" << std::endl;
+        return 1;
+    }
+
+    outFile2 << "Size Trand Treverse Tnear\n";
+    for (int i = 0; i < rand_vect.size(); ++i) {
+        long long rand_time = SortTester::averageTimeHMS(rand_vect[i], 20);
+        long long reverse_time = SortTester::averageTimeHMS(reverse_vect[i], 20);
+        long long near_time = SortTester::averageTimeHMS(near_vect[i], 20);
+
+        outFile2 << rand_vect[i].size() << " " << rand_time << " " << reverse_time << " " << near_time << "\n";
+    }
+    outFile2.close();
+    std::cout << "Результаты записаны в файл hms_results.txt" << std::endl;
     return 0;
 }
--- A2_additional/sort_tester.h
+++ A2_additional/sort_tester.h
@@ -12,19 +12,35 @@ class SortTester {
 public:
     static long long mSTime(const std::vector<int>& arr) {
         std::vector<int> copy = arr;
+
         auto start = std::chrono::high_resolution_clock::now();
         SortingAlg::mergeSort(copy, 0, copy.size() - 1);
-        auto elapsed = std::chrono::high_resolution_clock::now() - start;
-        long long msec = std::chrono::duration_cast<std::chrono::milliseconds> (elapsed).count();
-        return msec;
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        return duration.count();
     }
     static long long hybrMSTime(const std::vector<int>& arr, int threshold) {
         std::vector<int> copy = arr;
         auto start = std::chrono::high_resolution_clock::now();
         SortingAlg::hybridMergeSort(copy, 0, copy.size() - 1, threshold);
-        auto elapsed = std::chrono::high_resolution_clock::now() - start;
-        long long msec = std::chrono::duration_cast<std::chrono::milliseconds> (elapsed).count();
-        return msec;
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        return duration.count();
+    }
+    static long long averageTimeMS(const std::vector<int>& arr, int numSamples = 5) {
+        long long totalTime = 0;
+        for (int i = 0; i < numSamples; ++i) {
+            totalTime += mSTime(arr);
+        }
+        return totalTime / numSamples;
+    }
+    static long long averageTimeHMS(const std::vector<int>& arr, int threshold, int numSamples = 5) {
+        long long totalTime = 0;
+        for (int i = 0; i < numSamples; ++i) {
+            totalTime += hybrMSTime(arr, threshold);
+        }
+        return totalTime / numSamples;
     }
 };
 
